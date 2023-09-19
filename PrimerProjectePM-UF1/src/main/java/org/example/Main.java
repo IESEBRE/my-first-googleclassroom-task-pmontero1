38565d8f3b0140998b320e3fdeb3b46bc9a569d1
pmontero1 <pmontero@iesebre.com>
2023-09-19 13:15:10 +0200

    he afeigit el primer projecte de java del curs

---
+++ PrimerProjectePM-UF1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static int variable = 9;
+    static int variable2 = 10;
+    public static void main(String[] args) {
+        System.out.println(variable * variable2);
+        System.out.println(     );
+        System.out.println(variable2 / variable);
+        System.out.println(variable + variable2 );
+    }
+
+
+}
